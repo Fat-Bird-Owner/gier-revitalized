1541c50b4548fcaf7c1dddd06f8d739d2de9fc72
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-09 19:19:51 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,10 @@ Planets.gier.accessible = true;
 });
 
 Events.on(WorldLoadEvent, e => {
+
+    var parent = TechTree.all.find(u => u.content == StatusEffects.vow-gier);
     
-    if (Planets.gier.techTree == null) return;
-    
-    Planets.gier.techTree.each(node => {
+    parent.each(node => {
         if(node.content.unlocked){
             node.content.clearUnlock();
         }
