2f0430d3e4d76dadd82da0f05e83d5363273376f
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-09 20:45:40 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,19 @@ Planets.gier.accessible = true;
 });
 
 Events.on(SectorLaunchEvent, e => { 
-var attempt = e.info.attempts;
-e.generateEnemyBase = true;
-if (attempt == null) return;
-  
-Vars.ui.showInfoText("[lightgrey]Gier: The Asteroid Belt[]","Your current attempt count is " + attempt);
-});
 
+if (e.info == null) {
+Vars.ui.showInfoText("[lightgrey]Gier: The Asteroid Belt[]","The first of ever.);
+}else{
+Vars.ui.showInfoText("[lightgrey]Gier: The Asteroid Belt[]","Your current attempt count is " + e.info.attempts);
+}
+  
+
+});
+/*
 Events.on(WorldLoadEvent, event => {
 
   Vars.ui.showInfoText("[lightgrey]Gier: The Asteroid Belt[]","Your current attempt count is 0");
   
 })
+*/
