22ae4c783450111d8fd6354859f5640ee14579c2
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-09 20:31:13 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,9 @@ Planets.gier.accessible = true;
 
 Events.on(SectorLaunchEvent, e => { 
 var attempt = e.info.attempts;
+e.generateEnemyBase = true,
+if (attempt == null) return;
+  
 Vars.ui.showInfoText("[lightgrey]Gier: The Asteroid Belt[]","Your current attempt count is " + attempt);
 });
 
