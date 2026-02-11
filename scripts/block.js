a3e90bd655603eeaf5b72026ccbf9187f3e1e299
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-02-11 13:49:23 +0700

    Update block.js

--- scripts/block.js
+++ scripts/block.js
@@ -1,12 +1,1 @@
-const wall = extend(Wall, "terrible-conveyor", {
-
-  onDestroyed(tile){
-    Vars.ui.hudfrag.showToast("Pathetic.");
-    this.super$onDestroyed(tile);
-  }
-
-});
-
-// set stats AFTER creation
-wall.size = 3;
-wall.health = 200;
+const wall = extend(Wall, "terrible-conveyor", {});
