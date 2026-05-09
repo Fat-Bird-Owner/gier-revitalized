974e1d7005396d65a36cf544c29e32ffe04803cd
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-09 14:08:17 +0700

    Create transcript.js

---
+++ scripts/transcript.js
@@ -0,0 +1,30 @@
+function transcript(base, ref, prop){
+try{
+
+if(prop === ""){
+for(let k in ref){
+try{
+base[k] = ref[k];
+}catch(e){}
+}
+return;
+}
+
+if(Array.isArray(prop)){
+for(let i = 0; i < prop.length; i++){
+const key = prop[i];
+base[key] = ref[key];
+}
+return;
+}
+
+base[prop] = ref[prop];
+
+}catch(e){
+Vars.ui.showText("Transcript Error", e);
+}
+}
+
+module.exports = {
+transcript: transcript
+}
