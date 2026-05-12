39bc0bdf994952e6871d39c9076c8578177402f9
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-12 10:35:59 +0700

    Update Stats.js

--- scripts/Stats.js
+++ scripts/Stats.js
@@ -4,7 +4,9 @@ HealPercent: new Stat("healPerc", StatCat.function),
 CircuitRate: new Stat("circuitRate", StatCat.function),
 CircuitHeatDamage: new Stat("circuitHeatDamage", StatCat.function),
 CircuitRange: new Stat("circuitRange", StatCat.function),
-PistonPushLength: new Stat("pistonPushLength", StatCat.function)
+PistonPushLength: new Stat("pistonPushLength", StatCat.function),
+blastPower: new Stat("blastPower", StatCat.function),
+blastTier: new Stat("blastTier", StatCat.function)
 };
 
 module.exports = myStats;
