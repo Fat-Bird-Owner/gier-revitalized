69d037481bb6af58392254101119add5a7165826
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-12 14:02:00 +0700

    Update CombustionBarrel.js

--- scripts/CombustionBarrel.js
+++ scripts/CombustionBarrel.js
@@ -84,7 +84,7 @@ let len = 0;
 core.items.add(itemDrop, Mathf.round(amount));
 
 for (let i = 0; i < amount; i++){
-Time.runTask(0.05, () => {
+Time.runTask(0.05 * (i + 1), () => {
 Fx.itemTransfer.at(
 tile.worldx(),
 tile.worldy(),
