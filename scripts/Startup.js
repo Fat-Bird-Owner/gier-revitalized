04e44dd7910af34e18bfae2dc28cd6c2d52e3ffb
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-16 23:03:56 +0700

    Update Startup.js

--- scripts/Startup.js
+++ scripts/Startup.js
@@ -118,10 +118,12 @@ const block = Vars.content.block("gr-core-facility");
                 
 if (b == true){
 block.region = Core.atlas.find("gr-core-facility-old2");
+block.customShadowRegion = Core.atlas.find("gr-core-facility-shadow-old2");
 block.teamRegion = Core.atlas.find("gr-core-facility-team-old2");
 } else {
 block.region = Core.atlas.find("gr-core-facility");
 block.teamRegion = Core.atlas.find("gr-core-facility-team");
+block.customShadowRegion = Core.atlas.find("gr-core-facility-shadow");
 }
                 
 } catch(e){
@@ -149,6 +151,7 @@ Vars.content.liquid("gr-plague").viscosity = 0;
 if (Core.settings.getBool("core-facility-old") == true){        
 block.region = Core.atlas.find("gr-core-facility-old2");
 block.teamRegion = Core.atlas.find("gr-core-facility-team-old2");
+block.customShadowRegion = Core.atlas.find("gr-core-facility-shadow-old2");
 }
 
 if (Core.settings.getBool("command-block") == true){        
