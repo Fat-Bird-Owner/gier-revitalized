db321ef828363d5d3bfc3abbcbe2c249e5f3b3d3
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-25 22:04:33 +0700

    Create PowerGrid.js

---
+++ scripts/PowerGrid.js
@@ -0,0 +1,25 @@
+let time = 0;
+let group = null;
+
+events.run(Trigger.update, () => {
+try {
+
+time += Time.delta;
+if (time >= 60 || group == null) {
+time = 0;
+group = Groups.build.select(build => build.block == Vars.content.block("gr-power-grid"))
+}
+
+if (group == null) return;
+group.each(build => {
+try {
+const netIn = build.sense(LAccess.powerNetIn) - build.sense(LAccess.powerNetOut);
+if (netIn >= build.block.attributes.get(Attributes.get("netIn"))) build.kill();
+
+  
+} catch(e){}
+});
+  
+} catch(e){
+Vars.ui.showInfoToast(e,5);
+}});
