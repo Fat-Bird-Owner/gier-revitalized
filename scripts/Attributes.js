93bb80acaf65fb060aa302adbf7668a1eea3f636
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-25 21:58:33 +0700

    Update Attributes.js

--- scripts/Attributes.js
+++ scripts/Attributes.js
@@ -7,7 +7,8 @@ circuitRate: Attribute.add("circuitRate"),
 circuitRange: Attribute.add("circuitRange"),
 pistonPushLength: Attribute.add("pistonPushLength"),
 blastPower: Attribute.add("blastPower"),
-blastTier: Attribute.add("blastTier")
+blastTier: Attribute.add("blastTier"),
+netIn: Attribute.add("netIn")
 }
 
 module.exports = attributes;
