ec01815eaeb3736d539aed9e432526e8c56aecbc
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-05-27 20:34:24 +0700

    Update PowerGrid.js

--- scripts/PowerGrid.js
+++ scripts/PowerGrid.js
@@ -8,6 +8,8 @@ let group = null;
 let ls = 0;
 let block = null;
 let netIn = null;
+const max = 30;
+let current = 0;
 
 Events.run(Trigger.update, () => {
 try {
@@ -46,9 +48,22 @@ ls = Groups.build.size();
 }
 
 if (group == null) return;
-for(let i = 0; i < group.size; i++){ 
+
+let loop = 0;
+for(let i = current; i < group.size; i++){ 
 
 const build = group.get(i);
+if (i >= group.size) {
+i = 0;
+current = 0;
+}
+
+loop++;
+if (loop >= max){
+current = i;
+break;
+}
+
 const netInAtt = build.sense(LAccess.powerNetIn) - build.sense(LAccess.powerNetOut);
   
 if (netInAtt >= build.block.attributes.get(netIn)) {
