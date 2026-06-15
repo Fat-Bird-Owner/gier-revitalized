43e2013a72683d2bf448c207379d719d123cd6d6
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-06-15 11:53:44 +0700

    Create Darkness.js

---
+++ scripts/Darkness.js
@@ -0,0 +1,10 @@
+Events.on(ContentInitEvent, () => {
+try {
+
+const darkness = Vars.content.block("gr-darkness");
+darkness.region = Core.atlas.find("window-empty");
+darkness.hasShadow = false;
+
+} catch(e){
+Vars.ui.showText("e - Darkness", e);  
+}});
