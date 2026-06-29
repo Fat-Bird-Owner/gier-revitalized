5b0b4232e7b36818d6a20ce900c3b896e5ac4ea7
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-06-29 15:43:22 +0700

    Update damageShiftWall.js

--- scripts/damageShiftWall.js
+++ scripts/damageShiftWall.js
@@ -10,7 +10,7 @@ target = Vars.content.block("gr-refined-scrap-barracade");
 transitBlock = Vars.content.block("gr-scrap-piled-barracade");
 } 
 
-if (build && build.block != target && build.health > (build.block.health / 2)) return;
+if (build && build.block != target && build.health <= (build.block.health / 2) && build.health > 0) return;
 
 const altHealth = build.health
   
@@ -18,6 +18,25 @@ const {tile} = build;
 tile.setBlock(transitBlock, build.team, 0);
 tile.build.health = altHealth;
 
+const fx = Object.assign(new ParticleEffect(), {
+sizeFrom: 3,
+sizeTo: 0,
+region: Core.atlas.find("item-scrap"),
+particles: 7,
+lifetime: 130,
+length: 22,
+layer: 21,
+spin: Mathf.random(0.05, 0.1),
+colorTo: Color.valueOf("ffffff"),
+colorFrom: Color.valueOf("ffffff"),
+offset: offset,
+sizeInterp: Interp.pow10In,
+interp: Interp.pow10Out,
+cone: 25
+});
+
+fx.at(source.x, source.y, source.rotation());
+  
 } catch(e){
 Vars.ui.showInfoToast(e + "[red] - damageShiftWall",5);
 }});
