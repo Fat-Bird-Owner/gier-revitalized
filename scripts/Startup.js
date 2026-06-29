1423f7065b3e249a66edffc727599ad97b8b7299
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-06-29 22:53:04 +0700

    Update Startup.js

--- scripts/Startup.js
+++ scripts/Startup.js
@@ -61,6 +61,9 @@ Core.bundle.properties.put("database-tag.gier", String.fromCharCode(Iconc.comman
 
 const scrapTag = Core.bundle.get("database-tag.adv-scrap");
 Core.bundle.properties.put("database-tag.adv-scrap", Items.scrap.emoji() + " " + scrapTag);
+
+const depletedThoriumTag = Core.bundle.get("database-tag.adv-depleted-thorium");
+Core.bundle.properties.put("database-tag.adv-depleted-thorium", Items.scrap.emoji() + " " + depletedThoriumTag);
   
 } catch(e){
 Vars.ui.showText("Startup.js Crash", e);
