2e99db44d5f79f8ff669d4abbd1bf77b6233e704
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-06-29 15:40:16 +0700

    Fix syntax errors in damageShiftWall.js

--- scripts/damageShiftWall.js
+++ scripts/damageShiftWall.js
@@ -5,13 +5,19 @@ Events.on(BuildDamageEvent, event => {
 try {
 const {build, source} = event;
 
-if target == null || transitBlock = null {
+if (target == null || transitBlock = null) {
 target = Vars.content.block("gr-refined-scrap-barracade");
 transitBlock = Vars.content.block("gr-scrap-piled-barracade");
 } 
 
-if build.block != target return
+if (build && build.block != target && build.health > (build.block.health / 2)) return;
+
+const altHealth = build.health
   
+const {tile} = build;
+tile.setBlock(transitBlock, build.team, 0);
+tile.build.health = altHealth;
+
 } catch(e){
 Vars.ui.showInfoToast(e + "[red] - damageShiftWall",5);
 }});
