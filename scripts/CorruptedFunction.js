55cb8eb4476d5199fbc8844ca4fbc9bf033ce4b3
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-07-03 11:26:24 +0700

    Update CorruptedFunction.js

--- scripts/CorruptedFunction.js
+++ scripts/CorruptedFunction.js
@@ -1,20 +1,18 @@
 Events.on(UnitDamageEvent, event => {
 try{
 const {unit, bullet} = event;
-const effect = Vars.content.statusEffect("gr-corrupted")
+const effect = Vars.content.statusEffect("gr-corrupted");
   
 if (!unit || !bullet || !unit.hasEffect(effect)) return;
 
-Vars.ui.showInfoToast("Dam", 5);
-
 const {type} = unit;
 const chance = Mathf.clamp(
-100 - unit.health / type.health * 100 - 25,
+100 - unit.health / type.health * 100 - 50,
 0,
 100
 );
 
-if (Mathf.random(1, 100) <= chance) {
+if (Mathf.random(1, 100) <= chance && !unit.hasEffect(StatusEffects.boss)) {
 unit.team = bullet.team;
 unit.unapply(effect);
 Fx.teleport.at(unit.x, unit.y);
