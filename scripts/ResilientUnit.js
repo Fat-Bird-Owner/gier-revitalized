33c247aeb51f26068cc5ae4f6bb63a467fc074f5
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-07-09 21:56:17 +0700

    Create ResilientUnit.js

---
+++ scripts/ResilientUnit.js
@@ -0,0 +1,28 @@
+const resilientUnit = [
+"gr-fort"
+];
+
+Events.on(UnitDamageEvent, event => {
+try {
+
+const {unit, bullet} = event;
+if (!unit || !bullet) return;
+
+let found = false;
+for (let i = 0; i < resilientUnit.length; i++){
+if (unit.type == Vars.content.unit(resilientUnit[i])){
+found = true;
+return;
+}}
+
+if (found != true) return;
+
+const shieldGain = Mathf.clamp(bullet.damage - unit.armor, 0, unit.armor);
+if (shieldGain <= 0) return;
+
+Fx.absorb.at(unit.x, unit.y);
+unit.shield += shieldGain;
+  
+} catch(e){
+Vars.ui.showInfoToast(e + " - resilientUnit", 5);
+}});
