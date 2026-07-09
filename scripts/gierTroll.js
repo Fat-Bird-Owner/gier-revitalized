6f5914a48ca25593dd84829ea28a1ecbd230627e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-07-09 22:26:07 +0700

    Update gierTroll.js

--- scripts/gierTroll.js
+++ scripts/gierTroll.js
@@ -2,12 +2,14 @@ Events.on(ClientLoadEvent, () => {
 try {
 
 const sec = Planets.gier.sectors.get(0);
+/*
 if (sec.info.attempts < 15) {
 sec.preset.difficulty = Mathf.clamp(sec.info.wave / 2.5, 10, 1);
 return;
 }
-
-sec.preset.difficulty = SectorDifficulty.unreasonable;
+*/
+  
+if (sec.info.attempts < 15) sec.preset.difficulty = SectorDifficulty.unreasonable;
   
 } catch(e){
 Vars.ui.showInfoToast(e,5);  
