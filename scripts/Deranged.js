0821bec0e9e608c1d37e76f262d8c2e450d7477d
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-07-11 11:47:34 +0700

    Update Deranged.js

--- scripts/Deranged.js
+++ scripts/Deranged.js
@@ -1,13 +1,13 @@
 Events.on(WorldLoadEvent, () => {
 try{
-if (Core.settings.getBool("deranged") == true && Vars.state.isCampaign()){        
+if (Core.settings.getBool("deranged") == true && Vars.state.isCampaign() && Vars.state.rules.sector == Planets.gier.sectors.get(0)){        
 const rule = Vars.state.rules;
-rule.unitBuildSpeedMultiplier = 0.65
+rule.unitBuildSpeedMultiplier = 0.95
 rule.unitCrashDamageMultiplier = 1.5;
-rule.ghostBlocks = false;
-rule.deconstructRefundMultiplier = 0;
+rule.deconstructRefundMultiplier = 0.25;
 rule.fog = true;
 rule.lighting = true;
+Vars.state.wave = 5;
   
 }} catch(e){
 Vars.ui.showText("bruv",e);
