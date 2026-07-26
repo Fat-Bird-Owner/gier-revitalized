44a445d07a4e54f81748a5dcb9081188dcac01fd
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-07-26 13:19:55 +0700

    Update gier-to-gier.js

--- scripts/gier-to-gier.js
+++ scripts/gier-to-gier.js
@@ -6,7 +6,7 @@ const actualRealGier = Planets.gier;
 
 for (let i in actualRealGier){
 try {
-if (i == "sectors" || i == "grid" || i == "radius" || i == "startSector" || i == "minZoom" || i == "gridMesh" || i == "sectorApproxRadius") continue;
+if (i == "sectors" || i == "grid" || i == "radius" || i == "startSector" || i == "minZoom" || i == "gridMesh" || i == "gridMeshLoader") continue;
 gierWannabe[i] = actualRealGier[i];
 
 } catch(e){}
