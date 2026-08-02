0492b3a9440fd6c8b6dd575a5e3e8aa05dabd980
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-02 13:53:12 +0700

    Create DisableSwitch.js

---
+++ scripts/DisableSwitch.js
@@ -0,0 +1,17 @@
+Events.on(TapEvent, e => {
+try {
+const { player, tile } = e;
+
+if (!player || tile.block() != Vars.content.block("gr-manual-override")) return;
+if (!tile.team() || tile.team() != player.team()) return;
+
+const build = tile.build
+build.enabled = !build.enabled
+
+if (!build.front() || build.front.team != player.team()) return;
+build.front.enabled = build.enabled
+tile.block().clickSound.at(build.x, build.y)
+
+} catch(e){
+Vars.ui.showErrorMessage(e)
+}});
