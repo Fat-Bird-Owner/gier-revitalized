0455994dc9fc8447ceb64757022d4f510957008e
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-02 22:14:30 +0700

    Create WorldScript.js

---
+++ scripts/WorldScript.js
@@ -0,0 +1,15 @@
+Events.on(BlockDestroyEvent, e => {
+try {
+const tile = e.tile
+if (tile.block() != Vars.content.block("gr-world-script") || tile.team == Team.derelict) return;
+
+const string = tile.build.message.toString();
+let error = "[grey]No Errors"
+
+try { new Function(string) } catch(err) { error = "[red]Error:[] " + err }
+tile.setBlock(Vars.content.block("gr-world-script"))
+tile.build.message.append(error);
+  
+} catch(e){
+Vars.ui.showErrorMessage(e)  
+}});
