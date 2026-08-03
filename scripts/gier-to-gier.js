6ddf0111363468045f9e94bb39e82fbb771082af
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-03 20:47:24 +0700

    Update gier-to-gier.js

--- scripts/gier-to-gier.js
+++ scripts/gier-to-gier.js
@@ -19,7 +19,6 @@ gierWannabe.allowLaunchSchematics  = true;
 gierWannabe.allowLaunchLoadout = true;
 gierWannabe.prebuildBase = false;
 gierWannabe.allowLaunchToNumbered = false;
-gierWannabe.campaignRules.legacyLaunchPads = true;
     
 actualRealGier.techTree.each(node => {
 
