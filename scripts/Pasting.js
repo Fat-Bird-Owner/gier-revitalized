29ecfdf526a64e00a72a4230adff3d92c1aa3430
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-03 20:29:26 +0700

    Update Pasting.js

--- scripts/Pasting.js
+++ scripts/Pasting.js
@@ -1,6 +1,25 @@
 const pastes = [
 {
 from: "gr-export-silo",
-to: "
+to: "gr-landing-silo"
 }
 ]
+
+Events.on(ClientLoadEvent, () => {
+try {
+
+for (let i = 0; i < pastes.length; i++){
+let {from, to} = pastes[i]
+
+from = Vars.content.blocks(from)
+to = Vars.content.blocks(to)
+
+to.region = from.region;
+to.customShadowRegion = from.customShadowRegion;
+to.uiIcon = from.uiIcon;
+  
+}
+  
+} catch(e){
+Vars.ui.showErrorMessage(e) 
+}});
