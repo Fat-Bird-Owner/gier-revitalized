9822320faef755f26b95b0b8022bc5a946d6aecc
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-03 23:07:53 +0700

    Update CircuitLogic.js

--- scripts/CircuitLogic.js
+++ scripts/CircuitLogic.js
@@ -348,10 +348,16 @@ else ro = 1;
   
 if (b.tile.nearby(ro) == tile) {
   
-if (observerActivations >= 4) {
-Time.runTask(0.1 * 60, () => { try { runCircuit(b.tile)} } catch(e){} );
-} else {
-try { runCircuit(b.tile); } catch(e){}
+if(observerActivations >= 4){
+    Time.runTask(0.1 * 60, () => {
+        try{
+            runCircuit(b.tile);
+        }catch(e){}
+    });
+}else{
+    try{
+        runCircuit(b.tile);
+    }catch(e){}
 }
 
 observerActivations++;
