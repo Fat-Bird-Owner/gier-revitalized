2b65f185b6ce619c6a9ded2e983376d990857e12
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-13 10:22:35 +0700

    Update AttributeVanilla.js

--- scripts/AttributeVanilla.js
+++ scripts/AttributeVanilla.js
@@ -7,7 +7,7 @@ Blocks.stoneVent.attributes.set(Attribute.get("geothermal-source"), 1/2);
 Blocks.hotrock.attributes.set(Attribute.get("geothermal-source"), 0.5);
 Blocks.magmarock.attributes.set(Attribute.get("geothermal-source"), 0.75);
 
-Blocks.carbonWall.attributes.get(Attribute.get("carbon"), 1);
+Blocks.carbonWall.attributes.set(Attribute.get("carbon"), 1);
 Blocks.stoneWall.attributes.set(Attribute.get("beryllium"), 0.5);
   
 } catch(e){
