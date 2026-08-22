f9ab08532e30801fc4e7dbb7b8a40e92138a7350
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-22 21:00:51 +0700

    Create IconLoader.js

---
+++ scripts/IconLoader.js
@@ -0,0 +1,123 @@
+const extraIcons = new ObjectMap();
+
+function load(){
+try {
+  
+Log.info("Loading icons");
+addIcon("gier", "gr-gier");
+
+} catch(e){
+Log.err(e)
+}}
+
+function addIcon(iconName, regionName){
+try {
+  
+    extraIcons.put(iconName, regionName);
+
+} catch(e){
+Log.err(e)
+}}
+
+function packIcons(){
+try {
+  
+    var page = UI.packer.getPages().first();
+    var teams = Seq.with(Team.all);
+
+    extraIcons.each(function(entry){
+        var region = Core.atlas.find(entry.key);
+
+        if(!region.found()){
+            Log.warn("Could not find icon region: \"" + entry.key + "\"");
+            return;
+        }
+
+        page.setDirty(false);
+
+        var pixmapRegion = Core.atlas.getPixmap(region);
+
+        var team = teams.find(function(t){
+            return t.name == entry.value;
+        });
+
+        if(team != null){
+            var px = pixmapRegion.pixmap;
+
+            px.each(function(x, y){
+                px.setRaw(
+                    x,
+                    y,
+                    Color.muli(px.getRaw(x, y), team.color.rgba())
+                );
+            });
+
+            pixmapRegion.pixmap = px.outline(Pal.gray, 3);
+        }
+
+        var rect = UI.packer.pack(
+            region.name,
+            pixmapRegion,
+            region.splits,
+            region.pads
+        );
+
+        region.texture = page.getTexture();
+        region.set(
+            Mathf.int(rect.x),
+            Mathf.int(rect.y),
+            Mathf.int(rect.width),
+            Mathf.int(rect.height)
+        );
+
+        Core.atlas.getTextures().add(region.texture);
+        region.pixmapRegion = null;
+    });
+
+    page.setDirty(true);
+    page.updateTexture(
+        TextureFilter.linear,
+        TextureFilter.linear,
+        false
+    );
+
+} catch (e) {
+Log.err(e)
+}}
+
+function registerIcons(){
+try {
+  
+    var ch = 0xE001;
+
+    extraIcons.each(function(entry){
+        Fonts.registerIcon(
+            entry.key,
+            entry.value,
+            ch++,
+            Core.atlas.find(entry.value)
+        );
+    });
+
+} catch (e) {
+Log.err(e)
+}}
+
+Events.on(AtlasPackEvent, () => {
+try {
+
+    load();
+    packIcons();
+
+} catch(e){
+log.err("AtlasPack - " + e)
+}});
+
+Events.on(ClientLoadEvent, () => {
+try {
+  
+	  registerIcons();
+
+} catch(e){
+Log.err("Register - " + e)
+}});
