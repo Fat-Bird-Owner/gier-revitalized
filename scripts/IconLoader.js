90a14e6b629717cb455e3f983d4b1d6ffb0834a4
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-23 12:31:26 +0700

    Update IconLoader.js

--- scripts/IconLoader.js
+++ scripts/IconLoader.js
@@ -1,28 +1,33 @@
 const textures = [
-"gr-techtree-kela",
-"gr-gier",
-"gr-copper-fort",
-"gr-mass-launcher-base"
-]
+    "gr-techtree-kela",
+    "gr-gier",
+    "gr-copper-fort",
+    "gr-mass-launcher-base"
+];
 
 Events.on(ClientLoadEvent, () => {
-try {
+    let code = 0xE000;
 
-let code = 0xE000;
-	
-for (let i = 0; i < textures.length; i++){
-let texture = Core.atlas.find(textures[i]);
-	
-if (!texture.found()) {
-Log.err("Cant find[accent] " + textures[i] + "[] might not exist (Yet)");
-continue;
-}
+    for(let i = 0; i < textures.length; i++){
+        let name = textures[i];
+        let region = Core.atlas.find(name);
 
-Fonts.registerIcon(textures[i], texture.name, code + i, texture)
-Log.info("Registered: " + texture.name + " as " + Fonts.getUnicodeStr(textures[i]));
-	
-}
-	
-} catch(e){
-Log.err("AtlasPackEvent - " + e)
-}});
+        if(!region.found()){
+            Log.err("Icon texture not found: " + name);
+            continue;
+        }
+
+        Fonts.registerIcon(
+            name,
+            region.name,
+            code + i,
+            region
+        );
+
+        Log.info(
+            "Registered " + name +
+            " -> " + region.name +
+            " [" + (code + i) + "]"
+        );
+    }
+});
