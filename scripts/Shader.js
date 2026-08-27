958f93f1e53acbce4e1fb5b6e6722062701a82a6
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-08-27 14:17:43 +0700

    Update Shader.js

--- scripts/Shader.js
+++ scripts/Shader.js
@@ -1,31 +1,43 @@
+function loadShader(string){
+
 let g 
 
 try {
-
-g = new Shaders.SurfaceShader("test-water");
-
-} catch(e){}
+  
+g = new Shaders.SurfaceShader(string);
+  
+} catch(e){
+return null;
+}
 
 if (g != null){
-
+  
 let newLay = new CacheLayer.ShaderLayer(g)
 CacheLayer.add(0, newLay)
+return newLay;
+  
+} else{
+return null;
+}}
+
+let waterTest = loadShader("test-water");
+let heatedOil = loadShader("heated-oil");
 
 Events.on(ClientLoadEvent, () => {
 try {
 
-if (Core.settings.getBool("water-shader") == true){ 
-Blocks.water.cacheLayer = newLay;
-Blocks.deepwater.cacheLayer = newLay;
-Blocks.sandWater.cacheLayer = newLay;
-Blocks.taintedWater.cacheLayer = newLay;
-Blocks.darksandWater.cacheLayer = newLay;
-Blocks.deepTainedWater.cacheLayer = newLay;
-Blocks.darksandTaintedWater.cacheLayer = newLay;
+if (waterTest != null && Core.settings.getBool("water-shader") == true){ 
+Blocks.water.cacheLayer = waterTest;
+Blocks.deepwater.cacheLayer = waterTest;
+Blocks.sandWater.cacheLayer = waterTest;
+Blocks.taintedWater.cacheLayer = waterTest;
+Blocks.darksandWater.cacheLayer = waterTest;
+Blocks.deepTainedWater.cacheLayer = waterTest;
+Blocks.darksandTaintedWater.cacheLayer = waterTest;
 }
+
+if (heatedOil != null) Vars.content.block("gr-oil-tile").cacheLayer = heatedOil;
   
 } catch(e){
 log(e)
 }});
-
-}
