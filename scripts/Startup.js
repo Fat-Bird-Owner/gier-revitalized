4a5fcfe1ab85a1d9faf4bd9a8806ff413a4ca7b9
Fat Bird’s Owner <182750122+Fat-Bird-Owner@users.noreply.github.com>
2026-09-10 21:32:42 +0700

    Update Startup.js

--- scripts/Startup.js
+++ scripts/Startup.js
@@ -1,6 +1,6 @@
 function placePrefix(bundleName, prefix){
 let bundle = Core.bundle.get(bundleName);
-Core.bundle.properties.put(bundleName, prefix);
+Core.bundle.properties.put(prefix + bundleName);
 }
 
 Events.on(ContentInitEvent, e => { 
