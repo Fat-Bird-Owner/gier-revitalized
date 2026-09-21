a998869713acd65decdabeb63021cf30363403a1
Fat Bird’s Owner <182750122+Fat-Bird-Owner@users.noreply.github.com>
2026-09-21 22:53:01 +0700

    Update ResearchDialog.js

--- scripts/ResearchDialog.js
+++ scripts/ResearchDialog.js
@@ -216,7 +216,7 @@ log(e)
 } else {
 
 table.setColor((!valid(n) && bool) ? Pal.removeBack : Pal.darkestestGray);
-table.add(new Image(Icon.tree)).setColor((!valid(n) && bool) ? Color.black : Color.white);
+table.image((!valid(n) && bool) ? Icon.remove :Icon.tree)).color((!valid(n) && bool) ? Color.black : Color.white);
 
 }
 
