e885cad7eea4373feac56eb509153e225b814a13
Fat Bird’s Owner <182750122+Fat-Bird-Owner@users.noreply.github.com>
2026-09-26 11:17:58 +0700

    Update ProceduralGen.js

--- scripts/ProceduralGen.js
+++ scripts/ProceduralGen.js
@@ -114,7 +114,7 @@ let y = null;
 
 let wScale = wg.simplexNoise(0, 0)*1.5;
 let hScale = wg.simplexNoise(width-1, height-1)*1.5;
-Vars.world.size(Mathf.floor(width*wScale), Mathf.floor(height*hScale));
+Vars.world.resize(Mathf.floor(width*wScale), Mathf.floor(height*hScale));
 
 width = Mathf.floor(width*wScale);
 height = Mathf.floor(height*hScale);
